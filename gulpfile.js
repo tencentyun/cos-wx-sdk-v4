54d2cf1575279ccb7f69680a758f4417036a87ea
carsonxu <459452372@qq.com>
2017-07-19 22:54:09 +0800

    modify gulp dir

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,11 @@ gulp.task('default', ['rjs']);
 
 
 var concatTask = function () {
-	gulp.src(["../src/*.js"])
-		.pipe(uglify({
-			preserveComments:'license'
-		}))
+	gulp.src(["./src/*.js"])
+		.pipe(uglify({preserveComments: 'license'}))
 		.pipe(plugins.concat("cos-wx-sdk-v4.js"))
-		.pipe(gulp.dest("../samples/simple-app/lib/")) // 这是 demo 需要引用的文件
-		.pipe(gulp.dest("../dist").on('finish', function () {
+		.pipe(gulp.dest("./demo/lib/")) // 这是 demo 需要引用的文件
+		.pipe(gulp.dest("./dist/").on('finish', function () {
 			console.log('concat done...');
 		}));
 };
